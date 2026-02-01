8303b1d364eddb4c00ee8befadd1506eff13d30e
bibi <kevindaniel707070@gmail.com>
2026-02-01 11:02:35 -0400

    initial commit

---
+++ src/test/java/com/kdbf/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdbf.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
