490a38bc66d8c2d79ecd1560431f6446376c5b0a
bibi <kevindaniel707070@gmail.com>
2026-02-06 10:41:49 -0400

    chore(service): add missing import

--- src/main/java/com/kdbf/forum/application/port/in/RegisterTopicUseCase.java
+++ src/main/java/com/kdbf/forum/application/port/in/RegisterTopicUseCase.java
@@ -1,5 +1,7 @@
 package com.kdbf.forum.application.port.in;
 
+import com.kdbf.forum.application.domain.model.entity.Topic;
+
 public interface RegisterTopicUseCase {
   Topic registerTopic(RegisterTopicCommand command);
 }
