8e79fce448c3e16e7fd16e9aa66d980dc3f9611f
KDBatistaFiguereo <kevindaniel707070@gmail.com>
2026-06-14 20:54:16 -0400

    wip: working on search controller

---
+++ src/main/java/com/kdbf/forum/adapters/in/web/topic/SearchTopicController.java
@@ -0,0 +1,40 @@
+package com.kdbf.forum.adapters.in.web.topic;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kdbf.forum.adapters.in.web.topic.dto.ResponseTopicDto;
+import com.kdbf.forum.adapters.in.web.topic.mapper.TopicDtoMapper;
+import com.kdbf.forum.application.port.in.topic.SearchTopicUseCase;
+import com.kdbf.forum.application.port.in.topic.query.SearchTopicQuery;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class SearchTopicController {
+
+  SearchTopicUseCase searchTopicUseCase;
+  TopicDtoMapper topicDtoMapper;
+
+  @GetMapping("topics/search")
+  public ResponseEntity<List<ResponseTopicDto>> searchTopicsByTitle(
+      @RequestParam String searchTerm,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "20") int size) {
+
+    SearchTopicQuery query = new SearchTopicQuery(
+        searchTerm, page, size);
+
+    List<ResponseTopicDto> topics = searchTopicUseCase.searchTopic(query)
+        .stream()
+        .map(x -> topicDtoMapper.toDto(x))
+        .toList();
+
+    return ResponseEntity.ok(topics);
+  }
+}
